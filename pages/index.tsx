5ec75f42c174288071a28118c9412aeaee308abe
Vinicius Goncalves <vinicius.souza.goncalves@gmail.com>
2023-09-26 11:38:49 -0300

    configuração inicial do projeto

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import style from "../style/homeNotAuth.module.scss";
+
+
+const HomeNotAuth = () => {
+  return (
+		<>
+			<Head>
+        <title>Home</title>
+      </Head>
+			<main></main>
+		</>
+  );
+};
+
+export default HomeNotAuth;
